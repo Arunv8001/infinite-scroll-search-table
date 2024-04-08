aebc25c2dd548ab6e8377bfc9257cbdae9105610
Arunv8001 <a55001n@gmail.com>
2024-04-08 16:31:45 +0200

    Formatting the file

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,19 @@ import { fetchBooks } from "./api/apiCall.tsx";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import useDebounce from "./hooks/useDebounce.tsx";
 
-
 function App() {
   const [inputQuery, setInputQuery] = useState<string>("");
   const observer = useRef<IntersectionObserver | null>(null);
-
   const debouncedSearchTerm = useDebounce(inputQuery, 500);
 
   const { data, error, fetchNextPage, hasNextPage, isFetching, isLoading } =
     useInfiniteQuery({
       queryKey: ["inputQuery", debouncedSearchTerm],
-      queryFn: async ({pageParam}) => {
-        if(debouncedSearchTerm) {
-          return fetchBooks(debouncedSearchTerm, pageParam)
+      queryFn: async ({ pageParam }) => {
+        if (debouncedSearchTerm) {
+          return fetchBooks(debouncedSearchTerm, pageParam);
         }
-      return []
+        return [];
       },
       initialPageParam: 1,
       getNextPageParam: (lastPage, allPages) => {
@@ -27,57 +25,54 @@ function App() {
       },
     });
 
-    const lastElementRef = useCallback(
-      (node: HTMLDivElement) => {
-        if (isLoading) return;
-  
-        if (observer.current) observer.current.disconnect();
-  
-        observer.current = new IntersectionObserver((entries) => {
-          if (entries[0].isIntersecting && hasNextPage && !isFetching) {
-            fetchNextPage();
-          }
-        });
-  
-        if (node) observer.current.observe(node);
-      },
-      [fetchNextPage, hasNextPage, isFetching, isLoading]
-    );
-  
-    const books = useMemo(() => {
-      return data?.pages.reduce((acc, page) => {
-        return [...acc, ...page];
-      }, []);
-    }, [data]);
+  const lastElementRef = useCallback(
+    (node: HTMLDivElement) => {
+      if (isLoading) return;
 
+      if (observer.current) observer.current.disconnect();
 
-    if (error) return <h1>Erro occured</h1>;
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasNextPage && !isFetching) {
+          fetchNextPage();
+        }
+      });
 
+      if (node) observer.current.observe(node);
+    },
+    [fetchNextPage, hasNextPage, isFetching, isLoading]
+  );
+
+  const books = useMemo(() => {
+    return data?.pages.reduce((acc, page) => {
+      return [...acc, ...page];
+    }, []);
+  }, [data]);
+
+  if (error) return <h1>Erro occured</h1>;
 
   return (
     <>
-      <SearchInputComponent
-        setInputQuery={setInputQuery}
-      />
-      
+      <SearchInputComponent setInputQuery={setInputQuery} />
+
       <div>
-        {books && books?.map((value: string, index: number) => {
-          if (books?.length === (index + 1)) {
-            return (
-              <div ref={lastElementRef} className="search-title" key={index}>
-                {" "}
-                {value}
-              </div>
-            );
-          } else {
-            return (
-              <div className="search-title" key={index}>
-                {" "}
-                {value}
-              </div>
-            );
-          }
-        })}
+        {books &&
+          books?.map((value: string, index: number) => {
+            if (books?.length === index + 1) {
+              return (
+                <div ref={lastElementRef} className="search-title" key={index}>
+                  {" "}
+                  {value}
+                </div>
+              );
+            } else {
+              return (
+                <div className="search-title" key={index}>
+                  {" "}
+                  {value}
+                </div>
+              );
+            }
+          })}
       </div>
       <div>{isFetching && "Loading..."}</div>
     </>
